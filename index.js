5f1358c158d72ef0ff312898406c99f301ab7b49
clinomics-IamKira <ykkee7@clinomics.co.kr>
2020-06-11 09:44:36 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+const bodyParser = require("body-parser");
+const config = require("./config/key");
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(config.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("Mongo DB Connected....");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.get("/", (req, res) => res.send("Hello World?"));
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((error, result) => {
+    if (error) return res.json({ success: false, error });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
